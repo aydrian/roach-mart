905d79cae2e666be8ab15ff3ae6a73d0775b8000
Paul Scanlon <pauliescanlon@gmail.com>
2023-06-26 13:11:48 -0400

    feat/product-cart-cards (#2)
    * style: refactor product cards and main
    
    * removed .DS_Store file
    
    ---------
    
    Co-authored-by: Aydrian Howard <aydrian@gmail.com>

--- app/routes/_store+/_layout.tsx
+++ app/routes/_store+/_layout.tsx
@@ -1,5 +1,6 @@
 import { type DataFunctionArgs } from "@remix-run/node";
 import { Link, Outlet, useLoaderData } from "@remix-run/react";
+import { Fragment } from "react";
 
 import { Cart, Line, User } from "~/components/icons";
 import { GitHub } from "~/components/logos";
@@ -24,12 +25,12 @@ export default function StoreLayout() {
   const { user } = useLoaderData<typeof loader>();
   const cartItemCount = user?.cartItems?.length ?? 0;
   return (
-    <div className="grid min-h-screen grid-rows-[130px_auto_60px] bg-[#f5f5f5]">
-      <header className="grid grid-rows-[90px_40px]">
-        <div className="bg-white">
-          <div className="container flex h-full flex-col items-center justify-between md:flex-row">
-            <img alt="RoachMart" className="h-12 w-auto" src={logo} />
-            <div className="flex gap-4">
+    <Fragment>
+      <header className="fixed w-full">
+        <div className="flex min-h-[90px] items-center bg-white">
+          <div className="container flex h-full justify-between">
+            <img alt="RoachMart" className="h-6 w-auto sm:h-12" src={logo} />
+            <div className="flex items-center gap-4">
               <Link className="relative p-0.5" to="/cart">
                 <Cart className="inline-block h-6 w-auto text-[#637381]" />
                 {cartItemCount > 0 ? (
@@ -40,7 +41,7 @@ export default function StoreLayout() {
               </Link>
               <Line className="h-[30px] w-auto text-[#dde3e7]" />
               <div className="flex items-center gap-3">
-                <User className="inline-block h-6 w-auto text-black" />
+                <User className="hidden h-6 w-auto text-black sm:inline-block" />
                 {user ? (
                   <Link
                     className="font-medium text-crl-electric-purple"
@@ -60,14 +61,14 @@ export default function StoreLayout() {
             </div>
           </div>
         </div>
-        <div className="bg-crl-dark-blue font-medium text-white">
-          <div className="container flex h-full items-center justify-between">
+        <div className="flex min-h-[40px] items-center bg-crl-dark-blue font-medium text-white">
+          <div className="container flex justify-between">
             <Link to="/">Home</Link>
             {user ? <span>Welcome, {user.username}</span> : null}
           </div>
         </div>
       </header>
-      <main>
+      <main className="min-h-[calc(100vh-50px)] bg-[#f5f5f5] pt-[130px]">
         <Outlet />
       </main>
       <footer className="flex items-center bg-gradient-to-br from-crl-deep-purple from-45% via-crl-dark-blue to-crl-electric-purple p-2">
@@ -92,6 +93,6 @@ export default function StoreLayout() {
           </div>
         </div>
       </footer>
-    </div>
+    </Fragment>
   );
 }
--- app/routes/_store+/cart.tsx
+++ app/routes/_store+/cart.tsx
@@ -17,6 +17,7 @@ import {
 } from "~/components/ui/table";
 import { requireUserId } from "~/utils/auth.server";
 import { prisma } from "~/utils/db.server";
+import { Fragment } from "react";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "Roach Mart: Cart" }];
@@ -79,65 +80,126 @@ export default function ShoppingCart() {
           </h1>
         </div>
       </div>
-      <div className="container h-full w-full bg-[url('/images/bg-texture.svg')] bg-auto bg-left-top bg-no-repeat pt-10">
+      <div className="container h-full w-full bg-[url('/images/bg-texture.svg')] bg-auto bg-left-top bg-no-repeat py-10">
         {items.length > 0 ? (
-          <Table className="mx-auto mb-10 max-w-4xl border-separate border-spacing-x-0 border-spacing-y-8">
-            <TableBody>
-              {items.map((item) => (
-                <TableRow className="p-2" key={item.id}>
-                  <TableCell className="p-2">
-                    <img
-                      alt={item.product.name}
-                      className="h-24 w-auto rounded-sm"
-                      src={item.product.imgUrl}
-                    />
-                  </TableCell>
-                  <TableCell className="p-2 font-medium">
-                    <div>{item.product.name}</div>
-                    <div className="text-sm text-crl-electric-purple">
-                      {numFormat.format(Number(item.product.price.toString()))}
-                    </div>
-                  </TableCell>
-                  <TableCell className="border-x p-2 text-center text-sm text-[#959ead]">
-                    Expires in{" "}
-                    <CountdownTimer
-                      targetDate={
-                        item.expiration ? new Date(item.expiration) : new Date()
-                      }
-                    />
-                  </TableCell>
-                  <TableCell className="p-2">
-                    <Form className="flex justify-center" method="post" replace>
-                      <input name="id" type="hidden" value={item.id} />
-                      <Button
-                        aria-label="Delete Item"
-                        className="bg-[#d9d9d9] p-1"
-                        name="intent"
-                        size="sm"
-                        type="submit"
-                        value="deleteItem"
-                        variant="secondary"
-                      >
-                        <Trash className="h-6 w-auto" />
-                        <span className="sr-only">Delete</span>
-                      </Button>
-                    </Form>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-            <TableFooter className="bg-transparent">
-              <TableRow className="border-t">
-                <TableCell className="text-black">Total</TableCell>
-                <TableCell />
-                <TableCell />
-                <TableCell className="text-right text-crl-electric-purple">
-                  {numFormat.format(total)}
-                </TableCell>
-              </TableRow>
-            </TableFooter>
-          </Table>
+          <div className=" mx-auto max-w-5xl">
+            <ul className=" grid gap-8 lg:gap-4">
+              {items.map((item, index) => {
+                const {
+                  id,
+                  product: { name, price },
+                  expiration
+                } = item;
+                return (
+                  <Fragment key={index}>
+                    <li className="flex flex-col items-center justify-start gap-4 rounded-sm bg-white p-4 shadow md:justify-between lg:flex-row lg:gap-6 lg:bg-transparent lg:shadow-none ">
+                      <img
+                        alt={item.product.name}
+                        className="w-full rounded-sm lg:max-w-[160px]"
+                        src={item.product.imgUrl}
+                      />
+                      <div className="flex h-full w-full grow flex-col justify-center">
+                        <div>{name}</div>
+                        <div className="text-sm text-crl-electric-purple">
+                          {numFormat.format(Number(price.toString()))}
+                        </div>
+                      </div>
+                      <div className="flex h-full w-full items-center gap-2 lg:justify-center lg:border-x lg:border-x-gray-300">
+                        Expires in{" "}
+                        <CountdownTimer
+                          targetDate={
+                            expiration ? new Date(expiration) : new Date()
+                          }
+                        />
+                      </div>
+                      <div className="w-full lg:w-auto">
+                        <Form className="p-0 lg:px-8" method="post" replace>
+                          <input name="id" type="hidden" value={id} />
+                          <Button
+                            aria-label="Delete Item"
+                            className="w-full bg-[#d9d9d9] p-1 lg:w-auto"
+                            name="intent"
+                            size="sm"
+                            type="submit"
+                            value="deleteItem"
+                            variant="secondary"
+                          >
+                            <Trash className="h-6 w-auto" />
+                            <span className="block lg:hidden">Delete</span>
+                          </Button>
+                        </Form>
+                      </div>
+                    </li>
+                    <hr className="m-0 hidden lg:block" />
+                  </Fragment>
+                );
+              })}
+            </ul>
+            <div className="flex justify-between p-4 lg:p-8">
+              <strong>Total</strong>
+              <span className="text-sm text-crl-electric-purple">
+                {numFormat.format(total)}
+              </span>
+            </div>
+            <hr />
+          </div>
         ) : (
+          // <Table className="mx-auto mb-10 max-w-4xl border-separate border-spacing-x-0 border-spacing-y-8">
+          //   <TableBody>
+          //     {items.map((item) => (
+          //       <TableRow className="p-2" key={item.id}>
+          //         <TableCell className="p-2">
+          //           <img
+          //             alt={item.product.name}
+          //             className="h-24 w-auto rounded-sm"
+          //             src={item.product.imgUrl}
+          //           />
+          //         </TableCell>
+          //         <TableCell className="p-2 font-medium">
+          //           <div>{item.product.name}</div>
+          //           <div className="text-sm text-crl-electric-purple">
+          //             {numFormat.format(Number(item.product.price.toString()))}
+          //           </div>
+          //         </TableCell>
+          //         <TableCell className="border-x p-2 text-center text-sm text-[#959ead]">
+          //           Expires in{" "}
+          //           <CountdownTimer
+          //             targetDate={
+          //               item.expiration ? new Date(item.expiration) : new Date()
+          //             }
+          //           />
+          //         </TableCell>
+          //         <TableCell className="p-2">
+          //           <Form className="flex justify-center" method="post" replace>
+          //             <input name="id" type="hidden" value={item.id} />
+          //             <Button
+          //               aria-label="Delete Item"
+          //               className="bg-[#d9d9d9] p-1"
+          //               name="intent"
+          //               size="sm"
+          //               type="submit"
+          //               value="deleteItem"
+          //               variant="secondary"
+          //             >
+          //               <Trash className="h-6 w-auto" />
+          //               <span className="sr-only">Delete</span>
+          //             </Button>
+          //           </Form>
+          //         </TableCell>
+          //       </TableRow>
+          //     ))}
+          //   </TableBody>
+          //   <TableFooter className="bg-transparent">
+          //     <TableRow className="border-t">
+          //       <TableCell className="text-black">Total</TableCell>
+          //       <TableCell />
+          //       <TableCell />
+          //       <TableCell className="text-right text-crl-electric-purple">
+          //         {numFormat.format(total)}
+          //       </TableCell>
+          //     </TableRow>
+          //   </TableFooter>
+          // </Table>
           <div className="flex h-full w-full flex-col items-center gap-1.5">
             <Cart className="h-16 w-auto text-[#637381]" />
             <h2 className="text-center font-poppins text-2xl font-semibold text-crl-deep-purple">
